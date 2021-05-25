ee60d7a380e09b90e87cab03d3987ff38d3f2aa4
Kizito007 <nwakakelechi49@gmail.com>
2021-05-25 09:01:12 +0100

    Fetching Products from database

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,35 @@
+import express from "express"
+const router = express.Router()
+import Product from "../models/productModel.js"
+
+// @desc Fetch all Products
+// @route GET /api/products
+// @access Public
+
+router.get('/', async (req, res) => {
+    try {
+        const products = await Product.find({})
+        res.send(products);
+    } catch (err) {
+        res.status(500).send({ err: err.message })
+    }
+});
+
+// @desc Fetch single Product
+// @route GET /api/products/:id
+// @access Public
+
+router.get('/:id', async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id)
+        if (product) {
+            res.send(product);
+        } else {
+            res.status(404).json({ message: "Product not found" })
+        }
+    } catch (err) {
+        res.status(500).send({ err: err.message })
+    }
+});
+
+export default router
